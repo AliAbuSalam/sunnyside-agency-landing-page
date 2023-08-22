bef95a291beb6f3ebaae5489fccba8fc2b2cf0c0
Ali Abu Salam <aliadadehmav@gmail.com>
2023-08-22 20:48:36 +0700

    remove previous installation of fontawesome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/styles/global.scss';
 import type { Metadata } from 'next'
 import { Barlow } from 'next/font/google'
-import Script from 'next/script';
 
 import Navbar from '@/components/Navbar';
 
@@ -23,9 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={barlow.className}>
-        <Navbar />
         {children}
-        <Script src='https://kit.fontawesome.com/0c08249450.js' crossOrigin='anonymous'></Script>
       </body>
     </html>
   )
