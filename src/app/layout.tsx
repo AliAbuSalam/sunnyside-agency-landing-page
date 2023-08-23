d9080c8246063860438e46a4f63369847c35ecf8
Ali Abu Salam <aliadadehmav@gmail.com>
2023-08-23 21:49:06 +0700

    remove global font barlow.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={barlow.className}>
+      <body>
         <Navbar />
         {children}
       </body>
