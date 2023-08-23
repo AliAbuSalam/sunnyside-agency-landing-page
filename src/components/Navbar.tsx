6cf9ef5f889454b177d24214a4fd0a7e15b1be60
Ali Abu Salam <aliadadehmav@gmail.com>
2023-08-23 21:49:32 +0700

    add more styles to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
-import styles from '@/styles/Navbar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Barlow } from 'next/font/google';
+
+import styles from '@/styles/Navbar.module.scss';
+
+const barlow = Barlow({ 
+  subsets: ['latin'],
+  weight: '600', 
+})
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${barlow.className}`}>
       sunnyside
       <div>
-        <FontAwesomeIcon icon={faBars} />
+        <FontAwesomeIcon icon={faBars} size='lg'/>
       </div>
     </div>
   );
