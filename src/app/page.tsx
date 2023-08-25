d70b23c92cdf38b8b8348079e334a28b32e3555e
Ali Abu Salam <aliadadehmav@gmail.com>
2023-08-25 21:44:49 +0700

    create new grid on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import styles from "@/styles/page.module.scss";
+
 const Home = () => {
-  return(<div>header</div>);
+  return(
+    <div className={styles.container}>
+    </div>
+  );
 }
 
 export default Home;
