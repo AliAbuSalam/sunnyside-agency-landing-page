8f47ac1cb75396dd9b40c7c352138abe036e510e
Ali Abu Salam <aliadadehmav@gmail.com>
2023-09-21 19:55:32 +0700

    move Navbar component to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import styles from "@/styles/page.module.scss";
 
-import Navbar from "@/components/Navbar";
 import FirstBox from "@/components/FirstBox";
 
 const Home = () => {
   return(
     <div className={styles.container}>
       <div>
-        <Navbar />
         <FirstBox />
       </div>
     </div>
