18681c6e874a3b5ba3902d6358b89cc5c5df9adf
Ali Abu Salam <aliadadehmav@gmail.com>
2023-10-05 19:41:44 +0700

    Extract SecondBox component to a generic component

---
+++ src/components/BoxWithOnlyBackground.tsx
@@ -0,0 +1,26 @@
+'use client'
+import Image, { StaticImageData } from 'next/image';
+import { FC } from 'react';
+
+import styles from '@/styles/boxWithOnlyBackground.module.scss';
+
+interface boxProps {
+  imgSrc: StaticImageData;
+  alt: string;
+}
+
+const BoxWithOnlyBackground:FC<boxProps> = ({ imgSrc, alt }) => {
+  return(
+    <div className={styles['img-container']}>
+      <Image 
+        alt={alt}
+        src={imgSrc}
+        className={styles.img}
+        layout='responsive'
+        sizes='(max-width: 351px) 350px'
+      />
+    </div>
+  );
+};
+
+export default BoxWithOnlyBackground;
--- src/components/SecondBox.tsx
+++ src/components/SecondBox.tsx
@@ -1,19 +1,12 @@
-import Image from 'next/image';
-
+import BoxWithOnlyBackground from './BoxWithOnlyBackground';
 import Egg from '../../public/mobile/image-transform.jpg';
-import styles from '@/styles/secondBox.module.scss';
 
 const SecondBox = () => {
   return(
-    <div className={styles['img-container']}>
-      <Image 
-        alt='image of an egg'
-        src={Egg}
-        className={styles.img}
-        layout='responsive'
-        sizes='(max-width: 351px) 350px'
-      />
-    </div>
+    <BoxWithOnlyBackground 
+      imgSrc={Egg}
+      alt='image of an egg'
+    />
   );
 };
 
