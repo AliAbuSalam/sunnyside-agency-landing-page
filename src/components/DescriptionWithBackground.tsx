46b46cc0538ea2a9964b503d198cc41bb0ac2d8b
Ali Abu Salam <aliadadehmav@gmail.com>
2023-10-08 21:13:21 +0700

    Update DescriptionWithBackground component and style the component.

--- src/components/DescriptionWithBackground.tsx
+++ src/components/DescriptionWithBackground.tsx
@@ -1,22 +1,54 @@
 import React, { FC, ReactNode } from 'react';
-import { StaticImageData } from "next/image";
+import Image, { StaticImageData } from "next/image";
+import { Fraunces, Barlow } from 'next/font/google';
+
+import styles from '@/styles/descriptionWithBackground.module.scss';
+
+const fraunces = Fraunces({
+  subsets: ['latin'],
+  weight: '900',
+});
+
+const barlow = Barlow({
+  subsets: ['latin'],
+  weight: '600'
+})
 
 type DescWithBackgroundProps = {
   title: string;
   background: StaticImageData;
+  backgroundAlt: string;
   children: ReactNode;
+  textColor?: string;
 }
 
-const DescriptionWithBackground:FC<DescWithBackgroundProps> = ({ title, background, children }) => {
+
+const DescriptionWithBackground:FC<DescWithBackgroundProps> = ({ 
+  title, 
+  background,
+  backgroundAlt,
+  children,
+  textColor 
+  }) => {
   return(
-  <div>
-    <h1>
-      {title}
-    </h1>
-    <div>
-      {children}
+  <div className={styles.container}>
+    <Image 
+      src={background}
+      alt={backgroundAlt}
+      className={styles['background-image']}
+      width={350}
+      sizes='100vw'
+    />
+    <div className={styles['text-container']} style={{ color: textColor || 'black' }}>
+      <h2 className={fraunces.className}>
+        {title}
+      </h2>
+      <div className={`${barlow.className} ${styles.text}`}>
+        {children}
+      </div>
     </div>
-  </div>);
+  </div>
+  );
 };
 
 export default DescriptionWithBackground;
