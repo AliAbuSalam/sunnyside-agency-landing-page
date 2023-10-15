6b9aae86f8272d76150fc300273c410831027bac
Ali Abu Salam <aliadadehmav@gmail.com>
2023-10-15 19:18:33 +0700

    remove clients data from being hardcoded to component to an array outside of component

--- src/components/EighthBox.tsx
+++ src/components/EighthBox.tsx
@@ -9,42 +9,51 @@ import Jennie from '../../public/image-jennie.jpg';
 const fraunces = Fraunces({
   subsets: ['latin'],
   weight: '700'
-})
+});
+
+const clientsData = [
+  {
+    name: 'Emily R.',
+    profilePicture: Emily,
+    profileAlt: 'Picture of Emily',
+    position: 'Marketing Director',
+    testimony: <>We put our trust in Sunnyside and they delivered,
+      making sure our needs were met and deadlines were always hit.</>
+  },
+  {
+    name: 'Thomas S.',
+    profilePicture: Thomas,
+    profileAlt: 'Picture of Thomas',
+    position: 'Chief Operating Officer',
+    testimony: <>Sunnyside&apos;s enthusiasm coupled with their keen interest
+      in our brand&apos;s success made it a satisfying and enjoyable experience.</>
+  },
+  {
+    name: 'Jenny F.',
+    profilePicture: Jennie,
+    profileAlt: 'Picture of Jennie',
+    position: 'Business Owner',
+    testimony: <>Incredible end result! Our sales increased over 400% when we 
+      worked with Sunnyside. Highly recommended!</>
+  }
+]
 
 const EighthBox = () => {
   return(
     <div className={styles.container}>
       <h4 className={fraunces.className}>CLIENT TESTIMONIALS</h4>
       <div className={styles['testimonial-container']}>
-        <ClientTestimony
-          profilePicture={Emily}
-          profileAlt="picture of Emily"
-          name="Emily R."
-          position="Marketing Director"
-        >
-          We put our trust in Sunnyside and they delivered,
-          making sure our needs were met and deadlines
-          were always hit.
-        </ClientTestimony>
-        <ClientTestimony
-          profilePicture={Thomas}
-          profileAlt='picture of Thomas'
-          name='Thomas S.'
-          position='Chief Operating Officer'
-        >
-          Sunnyside&apos;s enthusiasm coupled with their keen interest
-          in our brand&apos;s success made it a satisfying and enjoyable
-          experience.
-        </ClientTestimony>
-        <ClientTestimony
-          profilePicture={Jennie}
-          profileAlt='picture of Jennie'
-          name='Jennie F.'
-          position='Business Owner'
-        >
-          Incredible end result! Our sales increased over 400% when we
-          worked with Sunnyside. Highly recommended!
-        </ClientTestimony>
+        {clientsData.map(client => (
+          <ClientTestimony
+            key={client.name}
+            name={client.name}
+            position={client.position}
+            profilePicture={client.profilePicture}
+            profileAlt={client.profileAlt}
+          >
+            {client.testimony}
+          </ClientTestimony>
+        ))}
       </div>
     </div>
   );
