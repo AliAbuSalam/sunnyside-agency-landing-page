39fc26e5a891e9d81a27f99326164a838e91628e
Ali Abu Salam <aliadadehmav@gmail.com>
2023-10-17 09:29:21 +0700

    create dropdown menu on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,16 +7,25 @@ import styles from '@/styles/Navbar.module.scss';
 const barlow = Barlow({ 
   subsets: ['latin'],
   weight: '600', 
-})
+});
+
+const dropdownOptions = ['About', 'Services', 'Projects', 'Contact'];
 
 const Navbar = () => {
   return (
-    <div className={`${styles.container} ${barlow.className}`}>
-      <span>
-        sunnyside
-      </span>
-      <div>
-        <FontAwesomeIcon icon={faBars} size='xl'/>
+    <div className={barlow.className}>
+      <div className={styles['nav-container']}>
+        <span>
+          sunnyside
+        </span>
+        <div>
+          <FontAwesomeIcon icon={faBars} size='xl'/>
+        </div>
+      </div>
+      <div className={styles.dropdown}>
+        {dropdownOptions.map(opt => (
+          <div key={opt} className={styles['dropdown-option']}>{opt}</div>
+        ))}
       </div>
     </div>
   );
