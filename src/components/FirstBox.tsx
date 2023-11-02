640c9dbcffd7ad5a8aa4369e282f21f02ee68447
Ali Abu Salam <aliadadehmav@gmail.com>
2023-11-03 03:37:39 +0700

    Change the image from using background image to using nextjs image component

--- src/components/FirstBox.tsx
+++ src/components/FirstBox.tsx
@@ -1,17 +1,33 @@
+import Image from 'next/image';
+
 import styles from '@/styles/firstBox.module.scss';
 import WhiteArrow from './WhiteArrow';
+import DesktopHeader from '../../public/desktop/image-header.jpg';
 import { fraunces } from '@/utils/fonts';
 
 const FirstBox = () => {
     return(
     <div className={styles['firstbox-container']}>
-      <h1 className={`${styles.h1} ${fraunces.className}`}>
-        WE ARE <br />CREATIVES
-      </h1>
-      <div className={styles['img-container']}>
-        <WhiteArrow 
-          className={styles.image}
+      <div className={styles['background-container']}>
+        <Image 
+          src={DesktopHeader}
+          alt='picture of oranges'
+          width={0}
+          height={0}
+          sizes='100vw'
+          className={styles.background}
+          style={{ width: '99vw', height: 'auto', zIndex: -1, }}
         />
+      </div>
+      <div className={styles.content}>
+        <h1 className={`${styles.h1} ${fraunces.className}`}>
+          WE ARE <br />CREATIVES
+        </h1>
+        <div className={styles['img-container']}>
+          <WhiteArrow 
+            className={styles.image}
+          />
+        </div>
       </div>
     </div>
   )
