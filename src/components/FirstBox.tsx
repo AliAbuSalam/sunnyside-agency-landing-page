68a6bb8498606a9c8885d6af5b84615a8db58842
Ali Abu Salam <aliadadehmav@gmail.com>
2023-11-05 20:39:04 +0700

    Add alternative image header for mobile and desktop using picture tag

--- src/components/FirstBox.tsx
+++ src/components/FirstBox.tsx
@@ -3,22 +3,28 @@ import Image from 'next/image';
 import styles from '@/styles/firstBox.module.scss';
 import WhiteArrow from './WhiteArrow';
 import DesktopHeader from '../../public/desktop/image-header.jpg';
+import MobileHeader from '../../public/mobile/image-header.jpg';
 import { fraunces } from '@/utils/fonts';
 import Navbar from './Navbar';
+
 
 const FirstBox = () => {
     return(
     <div className={styles['firstbox-container']}>
       <div className={styles['background-container']}>
-        <Image 
-          src={DesktopHeader}
-          alt='picture of oranges'
-          width={0}
-          height={0}
-          sizes='100vw'
-          className={styles.background}
-          style={{ width: '99vw', height: 'auto', zIndex: -1, }}
-        />
+        <picture>
+          <source srcSet={MobileHeader.src} media='(max-width: 400px)'/>
+          <Image 
+              src={DesktopHeader}
+              alt='picture of oranges'
+              width={0}
+              height={0}
+              sizes='100vw'
+              className={styles.background}
+              style={{ width: '100%', height: 'auto', zIndex: -1, }}
+            />
+        </picture>
+
       </div>
       <div className={styles.content}>
         <Navbar/>
