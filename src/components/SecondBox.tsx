d24610136efc445ad3b7f066bc0415b655b92140
Ali Abu Salam <aliadadehmav@gmail.com>
2023-11-09 18:19:05 +0700

    Add grid area for secondBox component

--- src/components/SecondBox.tsx
+++ src/components/SecondBox.tsx
@@ -1,12 +1,15 @@
 import BoxWithOnlyBackground from './BoxWithOnlyBackground';
 import Egg from '../../public/mobile/image-transform.jpg';
+import styles from '@/styles/secondBox.module.scss';
 
 const SecondBox = () => {
   return(
-    <BoxWithOnlyBackground 
-      imgSrc={Egg}
-      alt='image of an egg'
-    />
+    <div className={styles.container}>
+      <BoxWithOnlyBackground
+        imgSrc={Egg}
+        alt='image of an egg'
+      />
+    </div>
   );
 };
 
