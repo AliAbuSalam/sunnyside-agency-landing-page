d1aae60fea4fa5bebc596601af86456a8e4258cf
Ali Abu Salam <aliadadehmav@gmail.com>
2023-11-15 21:38:05 +0700

    add alternative images based on screen width for BoxWithOnlyBackground component

--- src/components/BoxWithOnlyBackground.tsx
+++ src/components/BoxWithOnlyBackground.tsx
@@ -4,20 +4,28 @@ import { FC } from 'react';
 import styles from '@/styles/boxWithOnlyBackground.module.scss';
 
 interface boxProps {
-  imgSrc: StaticImageData;
+  sourceSet?: {
+    img: StaticImageData;
+    media: string;
+  }[];
+  defaultImg: StaticImageData;
   alt: string;
 }
 
-const BoxWithOnlyBackground:FC<boxProps> = ({ imgSrc, alt }) => {
+const BoxWithOnlyBackground:FC<boxProps> = ({ defaultImg, alt, sourceSet }) => {
+  console.log('sourceSet: ', sourceSet)
   return(
     <div className={styles['img-container']}>
-      <Image 
-        alt={alt}
-        src={imgSrc}
-        className={styles.img}
-        sizes='(max-width: 351px) 350px'
-        style={{ width: '100%', height: 'auto' }}
-      />
+      <picture>
+        {sourceSet?.map((source, index) => <source srcSet={source.img.src} media={source.media} key={index}/>)}
+        <Image 
+          alt={alt}
+          src={defaultImg}
+          className={styles.img}
+          sizes='100vw'
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </picture>
     </div>
   );
 };
