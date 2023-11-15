69764d969cd4722d9cb7b89bf01d01c61ac9eb61
Ali Abu Salam <aliadadehmav@gmail.com>
2023-11-15 21:46:13 +0700

    add alternative image for fourthBox

--- src/components/FourthBox.tsx
+++ src/components/FourthBox.tsx
@@ -1,11 +1,20 @@
 import BoxWithOnlyBackground from "./BoxWithOnlyBackground";
 
 import Glass from '../../public/mobile/image-stand-out.jpg';
+import GlassDesktop from '../../public/desktop/image-stand-out.jpg';
+import { SCREEN_WIDTH_PHONE } from "@/lib/screenSizes";
+
 
 const FourthBox = () => {
+  const sourceSet = [{
+    img: Glass,
+    media: `(max-width: ${SCREEN_WIDTH_PHONE + 1}px)`
+  }];
+
   return(
     <BoxWithOnlyBackground 
-      imgSrc={Glass}
+      sourceSet={sourceSet}
+      defaultImg={GlassDesktop}
       alt="Image of a glass"
     />
   );
