1bf57e1594235965a382508e9852d6eb1b34105d
Ali Abu Salam <aliadadehmav@gmail.com>
2023-11-17 19:49:41 +0700

    add alternative background for desktop on seventhbox

--- src/components/SeventhBox.tsx
+++ src/components/SeventhBox.tsx
@@ -1,15 +1,22 @@
 import DescriptionWithBackground from "./DescriptionWithBackground";
 
 import Orange from '../../public/mobile/image-photography.jpg';
+import OrangeDesktop from '../../public/desktop/image-photography.jpg';
 import styles from '@/styles/seventhBox.module.scss';
+import { SCREEN_WIDTH_PHONE } from "@/lib/screenSizes";
 
 const SeventhBox = () => {
   const title = 'Photography';
   const backgroundAlt = 'Image of an orange';
+  const sourceSet = [{
+    image: Orange,
+    media: `(max-width: ${SCREEN_WIDTH_PHONE + 1}px)`
+  }]
   return(
     <DescriptionWithBackground 
       title={title}
-      background={Orange}
+      sourceSet={sourceSet}
+      defaultBackground={OrangeDesktop}
       backgroundAlt={backgroundAlt}
       textColor={styles.color}
     >
