83ecbc04ba5a61a5e7f909e492feafe8e870359f
Ali Abu Salam <aliadadehmav@gmail.com>
2023-11-17 19:49:14 +0700

    add alternative background for desktop on sixthBox

--- src/components/SixthBox.tsx
+++ src/components/SixthBox.tsx
@@ -1,14 +1,21 @@
 import DescriptionWithBackground from "./DescriptionWithBackground";
 
 import Cherry from '../../public/mobile/image-graphic-design.jpg';
+import CherryDesktop from '../../public/desktop/image-graphic-design.jpg';
 import styles from '@/styles/sixthBox.module.scss';
+import { SCREEN_WIDTH_PHONE } from "@/lib/screenSizes";
 
 const SixthBox = () => {
   const title = 'Graphic Design';
+  const sourceSet = [{
+    image: Cherry,
+    media: `(max-width: ${SCREEN_WIDTH_PHONE + 1}px)`
+  }];
   return(
     <DescriptionWithBackground
       title={title}
-      background={Cherry}
+      sourceSet={sourceSet}
+      defaultBackground={CherryDesktop}
       backgroundAlt="close up image of cherries"
       textColor={styles.color}
     >
