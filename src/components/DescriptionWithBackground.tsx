792344180eda446ea8a2f6090252ab645b88091d
Ali Abu Salam <aliadadehmav@gmail.com>
2023-11-17 19:48:42 +0700

    add sourceset function on descriptionwithbackground component

--- src/components/DescriptionWithBackground.tsx
+++ src/components/DescriptionWithBackground.tsx
@@ -6,7 +6,11 @@ import { fraunces } from '@/utils/fonts';
 
 type DescWithBackgroundProps = {
   title: string;
-  background: StaticImageData;
+  sourceSet?: {
+    image: StaticImageData,
+    media: string
+  }[];
+  defaultBackground: StaticImageData;
   backgroundAlt: string;
   children: ReactNode;
   textColor?: string;
@@ -14,21 +18,24 @@ type DescWithBackgroundProps = {
 
 
 const DescriptionWithBackground:FC<DescWithBackgroundProps> = ({ 
-  title, 
-  background,
+  title,
+  sourceSet,
+  defaultBackground,
   backgroundAlt,
   children,
   textColor 
   }) => {
   return(
   <div className={styles.container}>
-    <Image 
-      src={background}
-      alt={backgroundAlt}
-      className={styles['background-image']}
-      width={350}
-      sizes='100vw'
-    />
+    <picture className={styles['background-container']}>
+      {sourceSet?.map((source, index) => <source key={index} srcSet={source.image.src} media={source.media}/>)}
+      <Image 
+        src={defaultBackground}
+        alt={backgroundAlt}
+        className={styles['background-image']}
+        sizes='100vw'
+      />
+    </picture>
     <div className={styles['text-container']} style={{ color: textColor || 'black' }}>
       <h2 className={fraunces.className}>
         {title}
