8e4d0c71d2339454ad6b4e7e73a25ff253772cd1
Ali Abu Salam <aliadadehmav@gmail.com>
2023-12-14 16:34:29 +0700

    remove feature to close dropdown menu when blurred(still buggy) and add feature to close dropdown menu when link is clicked

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,17 +11,24 @@ const dropdownOptions = ['About', 'Services', 'Projects',];
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
-  
-  const handleMenuBlur = () => {
+
+  const handleLinkClick = () => {
     setToggleMenu(false);
   }
+  
+  // const handleMenuBlur = () => {
+  //   console.log('closed')
+  //   setToggleMenu(false);
+  // }
+  // NOT USED FOR NOW!!!!
 
-  useEffect(() => {
-    //focus the dropdown menu so it can be closed by firing on blur event when unfocused
-    if(toggleMenu){
-      dropdownRef.current?.focus();
-    }
-  }, [toggleMenu]);
+  // useEffect(() => {
+  //   //focus the dropdown menu so it can be closed by firing on blur event when unfocused
+  //   if(toggleMenu){
+  //     dropdownRef.current?.focus();
+  //   }
+  // }, [toggleMenu]);
+  // ALSO NOT USED FOR NOW!!!!!
 
   return (
     <div>
@@ -39,14 +46,20 @@ const Navbar = () => {
       <div 
         className={`${styles['dropdown-container']} ${!toggleMenu ? styles.hidden : ''}`}
         tabIndex={0}
-        onBlur={handleMenuBlur}
         id='MENU'
         ref={dropdownRef}
       >
         <div className={styles['dropdown-arrow']}></div>
         <div className={styles.dropdown}>
           {dropdownOptions.map(opt => (
-            <div key={opt} className={styles['dropdown-option']}>{opt}</div>
+            <a 
+              key={opt} 
+              className={styles['dropdown-option']} 
+              href={`#${opt.trim().toLowerCase()}`}
+              onClick={handleLinkClick}
+            >
+              {opt}
+            </a>
           ))}
           <div className={`${styles['dropdown-option']} ${styles['contact-button']} ${fraunces.className}`}>
             CONTACT
